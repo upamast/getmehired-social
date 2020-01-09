184115f5c7fb9bc959538e9858d0dec4fd91e9fd
upamast <59261755+upamast@users.noreply.github.com>
2020-01-09 17:05:24 -0600

    initial commit

---
+++ src/main/java/com/example/getmehiredsocial/GetmehiredSocialApplication.java
@@ -0,0 +1,13 @@
+package com.example.getmehiredsocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetmehiredSocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetmehiredSocialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/getmehiredsocial/GetmehiredSocialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.getmehiredsocial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetmehiredSocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
